4fe6b7c8d982e7dc6703cdd257fcf6be18713ef5
Yankeegsj <49223077+Yankeegsj@users.noreply.github.com>
2021-09-10 16:54:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 python ./code/test.py model=VGG_16_deconv_Fine_grained_DA_test gradient_scalar=-0.01 comment=99.3_vision_SHA \
 tar_dataset=SHA_test dataroot_SHA=./data/SHA vision_each_epoch=5000 \
-model_for_load=SHA_test.pth
+model_for_load=SHA_99.3.pth
